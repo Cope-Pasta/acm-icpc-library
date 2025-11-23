a3324838cff5a25beece0bb9513618dbda4a3ca1
Eduard Tykhoniuk <edik.tikhoniuk@gmail.com>
2025-11-23 14:21:39 +0100

    fix: use 0 index in graham

--- code/computational-geometry/convex-hull-2d.cpp
+++ code/computational-geometry/convex-hull-2d.cpp
@@ -2,19 +2,19 @@ bool cmp(const point_t p1, const point_t p2) {
 	return dblcmp(p1.y - p2.y) == 0 ? p1.x < p2.x : p1.y < p2.y;
 }
 
-int graham(point_t *p) { // Points co-lined are ignored.
-	int top = 2; static point_t sk[maxn];
-	sort(p + 1, p + 1 + n, cmp); 
-	sk[1] = p[1], sk[2] = p[2];
-	for (int i = 3; i <= n; ++i) {
-		while (top >= 2 && dblcmp(cross(p[i] - sk[top - 1], sk[top] - sk[top - 1])) >= 0) --top;
+int graham(vector<point_t>& p) { // Points co-lined are ignored.
+	int top = 1; static point_t sk[maxn];
+	sort(p, p + n, cmp);
+	sk[0] = p[0], sk[1] = p[1];
+	for (int i = 2; i < n; ++i) {
+		while (top >= 1 && dblcmp(cross(p[i] - sk[top - 1], sk[top] - sk[top - 1])) >= 0) --top;
 		sk[++top] = p[i];
 	}
 	int ttop = top;
-	for (int i = n - 1; i >= 1; --i) {
+	for (int i = n - 2; i >= 0; --i) {
 		while (top > ttop && dblcmp(cross(p[i] - sk[top - 1], sk[top] - sk[top - 1])) >= 0) --top;
 		sk[++top] = p[i];
 	}
-	for (int i = 1; i < top; ++i) p[i] = sk[i];
+	for (int i = 0; i < top; ++i) p[i] = sk[i];
 	return --top;
 }
